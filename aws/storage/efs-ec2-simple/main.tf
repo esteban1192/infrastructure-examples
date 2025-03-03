a4efab5b2cdbb1bfea1109aa0d9053257ee0a9b1
esteban1192 <estebanospinasaldarriaga@gmail.com>
2025-03-02 20:12:09 -0500

    config files

---
+++ aws/storage/efs-ec2-simple/main.tf
@@ -0,0 +1,50 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_subnet" "subnet" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+}
+
+resource "aws_security_group" "efs_sg" {
+  vpc_id = aws_vpc.main.id
+  
+  ingress {
+    from_port   = 2049
+    to_port     = 2049
+    protocol    = "tcp"
+    cidr_blocks = ["10.0.0.0/16"]
+  }
+}
+
+resource "aws_efs_file_system" "efs" {}
+
+resource "aws_efs_mount_target" "mount" {
+  file_system_id  = aws_efs_file_system.efs.id
+  subnet_id       = aws_subnet.subnet.id
+  security_groups = [aws_security_group.efs_sg.id]
+}
+
+resource "aws_instance" "ec2_instances" {
+  count         = 2
+  ami           = "ami-0c55b159cbfafe1f0" # Replace with your preferred AMI
+  instance_type = "t2.micro"
+  subnet_id     = aws_subnet.subnet.id
+  
+  user_data = <<-EOF
+              #!/bin/bash
+              yum install -y amazon-efs-utils
+              mkdir /mnt/efs
+              mount -t efs ${aws_efs_file_system.efs.id}:/ /mnt/efs
+              EOF
+  
+  tags = {
+    Name = "EC2-Instance-${count.index + 1}"
+  }
+}
---
+++ aws/storage/efs-ec2-simple/variables.tf
@@ -0,0 +1,9 @@
+variable "terraform_organization" {
+  type    = string
+  default = "your-organization"
+}
+
+variable "terraform_workspace" {
+  type    = string
+  default = "your-workspace"
+}
