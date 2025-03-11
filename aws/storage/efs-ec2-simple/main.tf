93aa08416689c731f7774a68908ee88304a4a4d5
esteban1192 <estebanospinasaldarriaga@gmail.com>
2025-03-11 14:44:24 -0500

    fmt

--- aws/storage/efs-ec2-simple/main.tf
+++ aws/storage/efs-ec2-simple/main.tf
@@ -1,20 +1,16 @@
-provider "aws" {
-  region = var.region
-}
-
 module "network" {
   source = "./modules/network"
 }
 
 module "storage" {
-  source = "./modules/storage"
+  source            = "./modules/storage"
   subnet_cidr_block = module.network.private_subnet_cidr_block
-  subnet_id = module.network.private_subnet_id
-  vpc_id = module.network.vpc_id
+  subnet_id         = module.network.private_subnet_id
+  vpc_id            = module.network.vpc_id
 }
 
 module "compute" {
-  source = "./modules/compute"
+  source    = "./modules/compute"
   subnet_id = module.network.private_subnet_id
-  efs_id = module.storage.efs_id
+  efs_id    = module.storage.efs_id
 }
