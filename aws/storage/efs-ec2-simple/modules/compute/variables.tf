0e41b44df16f556e27efbce84584944f019b3e05
esteban1192 <estebanospinasaldarriaga@gmail.com>
2025-03-11 14:44:55 -0500

    fmt

--- aws/storage/efs-ec2-simple/modules/compute/variables.tf
+++ aws/storage/efs-ec2-simple/modules/compute/variables.tf
@@ -1,19 +1,19 @@
 variable "instances_count" {
-    type = number
-    default = 2
+  type    = number
+  default = 2
 }
 
 variable "instances_type" {
-    type = string
-    default = "t3.micro"
+  type    = string
+  default = "t3.micro"
 }
 
 variable "subnet_id" {
-    type = string
-    description = "the id of the subnet in which the ec2 instances will be located"
+  type        = string
+  description = "the id of the subnet in which the ec2 instances will be located"
 }
 
 variable "efs_id" {
-    type = string
-    description = "the id of the efs to be mounted on the instances"
+  type        = string
+  description = "the id of the efs to be mounted on the instances"
 }
--- aws/storage/efs-ec2-simple/modules/storage/variables.tf
+++ aws/storage/efs-ec2-simple/modules/storage/variables.tf
@@ -1,14 +1,14 @@
 variable "vpc_id" {
-    description = "The VPC ID"
-    type = string
+  description = "The VPC ID"
+  type        = string
 }
 
 variable "subnet_cidr_block" {
-    description = "The cidr block of the subnet in which the efs will be located"
-    type = string
+  description = "The cidr block of the subnet in which the efs will be located"
+  type        = string
 }
 
 variable "subnet_id" {
-    description = "The ID of the subnet in which the efs will be located"
-    type = string
+  description = "The ID of the subnet in which the efs will be located"
+  type        = string
 }
