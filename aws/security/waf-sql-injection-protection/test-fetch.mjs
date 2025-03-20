3334509f6a528f1f3e03df2bdb96469ff6014218
esteban1192 <estebanospinasaldarriaga@gmail.com>
2025-03-20 11:27:13 -0500

    was-sql-injection-protection test fetch init commit

---
+++ aws/security/waf-sql-injection-protection/test-fetch.mjs
@@ -0,0 +1,18 @@
+async function testVulnerableEndpoint() {
+    const response = await fetch('https://hja2ve7mfk.execute-api.us-east-1.amazonaws.com/stage/sql-injection', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ "id": "1 UNION SELECT user_id, ssn from sensitive_data --" })
+    });
+  
+    console.log("Status:", response.status);
+  
+    const data = await response.json();
+  
+    console.log("Response Data:", data);
+  }
+  
+  testVulnerableEndpoint();
+  
