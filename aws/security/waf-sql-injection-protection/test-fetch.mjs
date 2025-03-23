f7f370aaf1905a5a975e2e1ca935008220caeea8
esteban1192 <estebanospinasaldarriaga@gmail.com>
2025-03-23 14:56:58 -0500

    waf-sql-injection-protection test fetch

--- aws/security/waf-sql-injection-protection/test-fetch.mjs
+++ aws/security/waf-sql-injection-protection/test-fetch.mjs
@@ -1,9 +1,10 @@
 async function testVulnerableEndpoint() {
-    const response = await fetch('https://hja2ve7mfk.execute-api.us-east-1.amazonaws.com/stage/sql-injection', {
+    const response = await fetch('https://7dkv8ymx90.execute-api.us-east-1.amazonaws.com/stage/sql-injection', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
       },
+      // body: JSON.stringify({ "id": "1" })
       body: JSON.stringify({ "id": "1 UNION SELECT user_id, ssn from sensitive_data --" })
     });
   
