bef8967ff706ab2ddc8ec1ef9659e7d22330b6c2
esteban1192 <estebanospinasaldarriaga@gmail.com>
2025-03-22 19:21:01 -0500

    waf-sql-injection-protection add region output

--- aws/security/waf-sql-injection-protection/outputs.tf
+++ aws/security/waf-sql-injection-protection/outputs.tf
@@ -9,3 +9,8 @@ output "deployment_id" {
 output "stage_name" {
   value = module.api_gateway.stage_name
 }
+
+output "deployment_region" {
+  description = "The region where the resources are deployed"
+  value       = var.region
+}
