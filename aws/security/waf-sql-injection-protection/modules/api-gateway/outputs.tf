6d0fca9c64d4febff9b23b47a3348f9b4823e71e
esteban1192 <estebanospinasaldarriaga@gmail.com>
2025-03-22 19:13:25 -0500

    api-gateway module outputs

---
+++ aws/security/waf-sql-injection-protection/modules/api-gateway/outputs.tf
@@ -0,0 +1,14 @@
+output "rest_api_id" {
+  description = "The ID of the API Gateway REST API"
+  value       = aws_api_gateway_rest_api.api.id
+}
+
+output "deployment_id" {
+  description = "The ID of the API Gateway deployment"
+  value       = aws_api_gateway_deployment.deployment.id
+}
+
+output "stage_name" {
+  description = "The name of the API Gateway stage"
+  value       = aws_api_gateway_stage.stage.stage_name
+}
