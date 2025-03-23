2cbdf5814764787cb7936a501e1fb50f9d1c91b5
esteban1192 <estebanospinasaldarriaga@gmail.com>
2025-03-22 19:13:06 -0500

    to not redeploy with triggers

--- aws/security/waf-sql-injection-protection/modules/api-gateway/main.tf
+++ aws/security/waf-sql-injection-protection/modules/api-gateway/main.tf
@@ -22,20 +22,12 @@ resource "aws_api_gateway_integration" "lambda_integration" {
   integration_http_method = "POST"
   type                    = "AWS_PROXY"
   uri                     = var.lambda_invoke_arn
+  depends_on = [ aws_api_gateway_method.lambda_function_method ]
 }
 
 resource "aws_api_gateway_deployment" "deployment" {
   rest_api_id = aws_api_gateway_rest_api.api.id
-  triggers = {
-    redeployment = sha1(jsonencode([
-      aws_api_gateway_resource.lambda_function_resource.id,
-      aws_api_gateway_method.lambda_function_method.id,
-      aws_api_gateway_integration.lambda_integration.id,
-    ]))
-  }
-  lifecycle {
-    create_before_destroy = true
-  }
+  depends_on = [ aws_api_gateway_integration.lambda_integration ]
 }
 
 resource "aws_lambda_permission" "apigw_invoke" {
@@ -44,7 +36,7 @@ resource "aws_lambda_permission" "apigw_invoke" {
   function_name = var.lambda_function_name
   principal     = "apigateway.amazonaws.com"
 
-  source_arn = "${aws_api_gateway_rest_api.api.execution_arn}/*/*"
+  source_arn = "${aws_api_gateway_rest_api.api.execution_arn}"
 }
 
 resource "aws_api_gateway_stage" "stage" {
