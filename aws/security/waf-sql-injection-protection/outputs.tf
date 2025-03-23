8257765edb61ca9a89d60b4c7e030c20822a73bb
esteban1192 <estebanospinasaldarriaga@gmail.com>
2025-03-22 19:12:37 -0500

    main outputs

---
+++ aws/security/waf-sql-injection-protection/outputs.tf
@@ -0,0 +1,11 @@
+output "rest_api_id" {
+  value = module.api_gateway.rest_api_id
+}
+
+output "deployment_id" {
+  value = module.api_gateway.deployment_id
+}
+
+output "stage_name" {
+  value = module.api_gateway.stage_name
+}
