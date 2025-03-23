63d3a6e4ea20c9dbb2ff331226e361ed8b85cbb4
esteban1192 <estebanospinasaldarriaga@gmail.com>
2025-03-23 00:34:23 -0500

    waf-sql-injection-protection POST method

--- aws/security/waf-sql-injection-protection/modules/api-gateway/main.tf
+++ aws/security/waf-sql-injection-protection/modules/api-gateway/main.tf
@@ -11,7 +11,7 @@ resource "aws_api_gateway_resource" "lambda_function_resource" {
 resource "aws_api_gateway_method" "lambda_function_method" {
   rest_api_id   = aws_api_gateway_rest_api.api.id
   resource_id   = aws_api_gateway_resource.lambda_function_resource.id
-  http_method   = "GET"
+  http_method   = "POST"
   authorization = "NONE"
 }
 
@@ -19,7 +19,7 @@ resource "aws_api_gateway_integration" "lambda_integration" {
   rest_api_id             = aws_api_gateway_rest_api.api.id
   resource_id             = aws_api_gateway_resource.lambda_function_resource.id
   http_method             = aws_api_gateway_method.lambda_function_method.http_method
-  integration_http_method = "GET"
+  integration_http_method = "POST"
   type                    = "AWS_PROXY"
   uri                     = var.lambda_invoke_arn
   depends_on = [ aws_api_gateway_method.lambda_function_method ]
