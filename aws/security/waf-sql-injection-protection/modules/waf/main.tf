8f3e30756dbb18eff0d7ae03cb35443a8d788935
esteban1192 <estebanospinasaldarriaga@gmail.com>
2025-03-23 19:46:44 -0500

    waf-sql-injection-protection block rule

--- aws/security/waf-sql-injection-protection/modules/waf/main.tf
+++ aws/security/waf-sql-injection-protection/modules/waf/main.tf
@@ -11,6 +11,10 @@ resource "aws_wafv2_web_acl" "api_waf" {
     name     = "SQLInjectionProtection"
     priority = 1
 
+    action {
+      block {}
+    }
+
     statement {
       managed_rule_group_statement {
         name        = "AWSManagedRulesSQLiRuleSet"
