9b3c5a28162fea8c523fda7416bee84edf6b88a2
esteban1192 <estebanospinasaldarriaga@gmail.com>
2025-03-25 12:44:41 -0500

    format test-fetch

--- aws/security/waf-sql-injection-protection/test-fetch.mjs
+++ aws/security/waf-sql-injection-protection/test-fetch.mjs
@@ -1,19 +1,18 @@
 async function testVulnerableEndpoint() {
-    const response = await fetch('https://7dkv8ymx90.execute-api.us-east-1.amazonaws.com/stage/sql-injection', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      // body: JSON.stringify({ "id": "1" })
-      body: JSON.stringify({ "id": "1 UNION SELECT user_id, ssn from sensitive_data --" })
-    });
-  
-    console.log("Status:", response.status);
-  
-    const data = await response.json();
-  
-    console.log("Response Data:", data);
-  }
-  
-  testVulnerableEndpoint();
-  
+  const response = await fetch('https://bqfu3z5zol.execute-api.us-east-1.amazonaws.com/stage/sql-injection', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ "id": "1" })
+    // body: JSON.stringify({ "id": "1 UNION SELECT user_id, ssn from sensitive_data --" })
+  });
+
+  console.log("Status:", response.status);
+
+  const data = await response.json();
+
+  console.log("Response Data:", data);
+}
+
+testVulnerableEndpoint();
